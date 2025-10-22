7df685f31ea32aba095e2bab4f2dadf1557f3e66
Tine Staric <tine.staric@companial.com>
2025-10-22 15:44:57 +0300

    Revert "refactor: Simplify color scheme management in global styles and layout"
    This reverts commit b8798d069c779ea8ece16ab72e55f6542a48e406.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,10 +40,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" style={{ colorScheme: 'light' }}>
+    <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover" />
-        <meta name="color-scheme" content="light only" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="mobile-web-app-capable" content="yes" />
